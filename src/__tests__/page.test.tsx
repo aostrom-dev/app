668f9b41b0194b3ca99f6cdac117566f14ce76c8
Alexandre Philibert <alexandre.philibert@ik.me>
2024-06-03 15:42:06 +0200

    Add test

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+test("Home", () => {
+  render(<Home />);
+
+  const h1 = screen.getByRole("heading");
+
+  expect(h1).toBeDefined();
+  expect(h1.textContent).toContain("blue");
+});
