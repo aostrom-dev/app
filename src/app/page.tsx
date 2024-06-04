2bfdc25220442d0f4e348c46fccfe028a9461081
Alexandre Philibert <alexandre.philibert@ik.me>
2024-06-04 18:32:57 +0200

    Fix mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
   return (
     <main
       className={cn(
-        "min-h-screen grid place-items-center text-3xl text-white",
+        "overflow-auto min-h-screen grid place-items-center text-3xl text-white",
         {
           "bg-red-500": color === "red",
           "bg-blue-500": color === "blue",
         }
       )}
     >
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-8 m-6">
         <h1 className="uppercase font-bold">{color}</h1>
         <p>{process.env.NEXT_PUBLIC_GIT_HASH}</p>
       </div>
